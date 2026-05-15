57b3b2f287905127490e59b955eb76defe6506d2
axelroman-31 <a25110289@ceti.mx>
2026-05-15 00:30:01 +0000

    practica 8

--- include/EstadoFoco.hpp
+++ include/EstadoFoco.hpp
@@ -1,5 +1,4 @@
 #pragma once
 #include <string>
 
-typedef bool EstadoFoco;
 typedef std::string EstadoFoco;
--- src/main.cpp
+++ src/main.cpp
@@ -6,15 +6,11 @@ using namespace std;
 
 int main(int argc, char const *argv[]) 
 {
-    Foco foquito;
-    foquito.Encender();
-    foquito.Apagar();
     SerieFocos serie;
     serie.EncenderFocos();
     serie.MostrarFocos();
     serie.ApagarFocos();
     serie.MostrarFocos();
 
-    cout << "Estado Foco:  " << foquito.LeerEstado() << endl;
     return 0;
 }
